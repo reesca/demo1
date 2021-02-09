59c1c55c18332558299e4b285d0af24a57f84acc
reesca <reesca@users.noreply.github.com>
2021-02-08 22:42:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
 
-echo "hi Mom";
+/*
+    "name": "BenchmarkDotNet",
+    "html_url": "https://github.com/dotnet/BenchmarkDotNet",
+    "description": "Powerful .NET library for benchmarking",
+    "homepage": "https://benchmarkdotnet.org",
+    "watchers": 6198,
+*/
+
+$arr = [];
+
+$item['name'] = "TestName1";
+$item['description'] = "TestDescription1";
+$item['html_url'] = "http://charlierees.com/TestURL1";
+$item['homepage'] = "http://charlierees.com";
+$item['watchers'] = 10;
+$arr[] = $item;
+
+$item['name'] = "TestName2";
+$item['description'] = "TestDescription2";
+$item['html_url'] = "http://charlierees.com/TestURL2";
+$item['homepage'] = "http://charlierees.com";
+$item['watchers'] = 11;
+$arr[] = $item;
+
+echo json_encode($arr);
 
 ?>
 
